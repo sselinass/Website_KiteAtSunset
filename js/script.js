6276f3fe7b71795fed2369e4bd6e67d58b8cfeb6
Selina Schöpfer <selina.schoepfer@stud.fhgr.ch>
2025-05-26 16:40:20 +0200

    icon

--- js/script.js
+++ js/script.js
@@ -47,35 +47,35 @@ const spotInfo = [
       lat: 54.0,
       lng: 10.0,
       name: "Kiel, DE",
-      description: "xxxx",
+      description: "heute bei Sonnenuntergang in Kiel erwartet dich:",
       image: "assets/img/hamburg_n.jpeg"
     },
     {
       lat: 46.4,
       lng: 9.68,
       name: "Silvaplana, CH",
-      description: "xxxx",
+      description: "heute bei Sonnenuntergang in Silvaplana erwartet dich:",
       image: "assets/img/silvaplana_n.jpeg"
     },
     {
       lat: 28.3,
       lng: -14.0,
       name: "Fuerteventura, ES",
-      description: "xxxx",
+      description: "heute bei Sonnenuntergang in Fuerteventura erwartet dich:",
       image: "assets/img/fuerteventura_n.jpeg"
     },
     {
       lat: 43.0,
       lng: 3.0,
       name: "Leucate, FRA",
-      description: "xxxx",
+      description: "heute bei Sonnenuntergang in Leucate erwartet dich:",
       image: "assets/img/narbonne_n.jpeg"
     },
     {
       lat: 53.0,
       lng: -9.0,
       name: "Galway, IRL",
-      description: "xxxx",
+      description: "heute bei Sonnenuntergang in Galway erwartet dich:",
       image: "assets/img/galway_n.jpeg"
     }
   ];
@@ -165,10 +165,18 @@ L.tileLayer('https://{s}.basemaps.cartocdn.com/dark_nolabels/{z}/{x}/{y}{r}.png'
 
   
 // markers
+const customIcon = L.icon({
+    iconUrl: 'assets/img/kiteschirm.png', 
+    iconSize: [35, 35],                  
+    iconAnchor: [16, 32],                // point of the icon which corresponds to the marker's location
+    popupAnchor: [0, -32]                // point from which the popup should open relative to the iconAnchor
+  });
+
+
 sortedData.forEach(spot => {
     const popupContent = `
         <strong>${spot.name}</strong><br>
-        <img src="${spot.image}" alt="${spot.name}" style="width: 100%; max-height: 100px; object-fit: cover; border-radius: 8px;" /><br>
+        <img src="${spot.image}" alt="${spot.name}" style="width: 100%; max-height: 180px; object-fit: cover; border-radius: 8px;" /><br>
         <em>${spot.description}</em><br><br>
         Temperatur: ${spot.temperatureActual}°C<br>
         Wind: ${spot.windSpeedActual} km/h<br>
@@ -176,7 +184,7 @@ sortedData.forEach(spot => {
         Sonnenuntergang: ${spot.sunset}
     `;
 
-    L.marker([spot.latitude, spot.longitude])
+    L.marker([spot.latitude, spot.longitude], {icon: customIcon})
         .addTo(map)
         .bindPopup(popupContent);
 });
